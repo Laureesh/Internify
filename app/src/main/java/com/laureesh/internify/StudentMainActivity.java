2ae2e0a11a67e65267e29780617abd6a4a9af3a0
Jw121065 <jwhelan1@ggc.edu>
2026-04-09 18:26:22 -0400

    Update StudentMainActivity.java

--- app/src/main/java/com/laureesh/internify/StudentMainActivity.java
+++ app/src/main/java/com/laureesh/internify/StudentMainActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentMainActivity extends AppCompatActivity {
+
+    Button aboutBtn;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+
+        aboutBtn = findViewById(R.id.aboutBtn);
+
+        aboutBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, AboutActivity.class));
+        });
     }
 }
