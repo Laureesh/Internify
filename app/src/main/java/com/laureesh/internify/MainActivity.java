d2a9bcbcd9ea7b5bfca19a37e1a263069f5a69e8
Jw121065 <jwhelan1@ggc.edu>
2026-04-09 18:00:47 -0400

    initial commit

--- app/src/main/java/com/laureesh/internify/MainActivity.java
+++ app/src/main/java/com/laureesh/internify/MainActivity.java
@@ -1,24 +1,37 @@
-package com.laureesh.internify;
+package jw.internify;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email, password;
+    Button loginBtn, goToRegisterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        goToRegisterBtn = findViewById(R.id.goToRegisterBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            String userEmail = email.getText().toString();
+
+            if (userEmail.contains("student")) {
+                startActivity(new Intent(this, StudentMainActivity.class));
+            } else {
+                startActivity(new Intent(this, CompanyMainActivity.class));
+            }
+        });
+
+        goToRegisterBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterActivity.class));
         });
     }
 }
